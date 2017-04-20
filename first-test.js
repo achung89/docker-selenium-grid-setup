58ced4518e1f3b18532ffb62a8c849b16c32396b
Andrew Chung <chung.andrew7@gmail.com>
2017-04-20 00:46:19 -0700

    first commit

---
+++ first-test.js
@@ -0,0 +1,31 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder()
+   .forBrowser('firefox')
+   .usingServer('http://localhost:4444/wd/hub')
+   .build();
+driver.get('http://www.google.com');
+
+var element = driver.findElement(webdriver.By.name('q'));
+element.sendKeys('Cheese!');
+element.submit();
+
+driver.getTitle().then(function(title) {
+  console.log('Page title is: ' + title);
+});
+
+driver.wait(function() {
+  return driver.getTitle().then(function(title) {
+    return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
+  });
+}, 3000);
+
+driver.getTitle().then(function(title) {
+  console.log('Page title is: ' + title);
+});
+
+driver.quit();
+// driver.get('http://www.google.com');
+// driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
+// driver.findElement(webdriver.By.name('btnG')).click();
+// driver.quit();
